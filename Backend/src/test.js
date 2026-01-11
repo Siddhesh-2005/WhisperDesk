56bb0e7b155ace896e52cbb7d5b745e12db4f47c
Siddhesh-2005 <siddheshbagde123@gmail.com>
2026-01-11 15:49:44 +0530

    Remove OpenAI moderation test code

--- Backend/src/test.js
+++
@@ -1,9 +0,0 @@
-import OpenAI from "openai";
-const openai = new OpenAI({
-    apiKey:"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"
-});
-
-const moderation = await openai.moderations.create({
-    model: "omni-moderation-latest",
-    input: "",
-});
