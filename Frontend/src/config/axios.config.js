5c74ee0c47445bd51087d7f2d49edc3244524729
Siddhesh-2005 <siddheshbagde123@gmail.com>
2026-01-19 20:37:08 +0530

    Add missing 'Accept' header in axios instance configuration

--- Frontend/src/config/axios.config.js
+++ Frontend/src/config/axios.config.js
@@ -9,6 +9,7 @@ const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
+    'Accept': 'application/json',
   },
   withCredentials: true, // Important for cookies/sessions
 });
