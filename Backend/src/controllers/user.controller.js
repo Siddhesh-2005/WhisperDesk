62a3ce0488cc2a41cab98cbc9696710606d10acd
Siddhesh-2005 <siddheshbagde123@gmail.com>
2026-01-19 19:48:08 +0530

    Fix sendEmail response - include rawToken in ApiResponse

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -51,7 +51,7 @@ const sendEmail = asyncHandler(async (req, res) => {
 
     return res
         .status(200)
-        .json(new ApiResponse(200, "Email sent successfully"));
+        .json(new ApiResponse(200,rawToken, "Email sent successfully"));
 });
 
 const login = asyncHandler(async (req, res) => {
