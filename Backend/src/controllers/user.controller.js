7a0e90ac12eb8d6ceb0ea099c5d645f9a729d181
Siddhesh-2005 <siddheshbagde123@gmail.com>
2026-01-22 21:47:19 +0530

    Refactor email sending import to use updated mailer module

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -1,7 +1,7 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
-import { sendEmail as send } from "../utils/mailer.resend.js";
+import { sendEmail as send } from "../utils/mailer.js";
 import { validateCollegeEmail } from "../services/validateEmail.service.js";
 import { User } from "../models/user.model.js";
 import { generateUniqueUsername } from "../services/generateUsername.service.js";
